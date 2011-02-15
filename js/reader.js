4d523ef0f603ebcf1ec84fb991053467cb43b373
Tate Tian <tatetian@gmail.com>
2011-02-15 16:19:50 +0800

    Add some visual effect for controls
    Controls are able to correspond to hover and click event

--- js/reader.js
+++ js/reader.js
@@ -2,6 +2,8 @@
  * init elements on load event  
  * */
 $(document).ready(function(){
+	// add event handlers to controls
+	addEventHandlerToControls() ;
 	// load page content
 	// step 1: load css for content
 	var $css = $('<link href="/content/content.css" rel="stylesheet" media="all" />') ;
@@ -17,6 +19,26 @@ $(document).ready(function(){
 		//$(this).unbind("load") ;
 	}) ;
 }) ;
+
+function addEventHandlerToControls() {
+	//alert($(".c"))
+	$(".control").hover(function() {//enter
+		$(this).css("opacity","1.0") ;
+		$(this).css("filter", "alpha(opacity=100)") ;
+		$(this).css("cursor","pointer");
+	}, function() {//leave
+		$(this).css("opacity","0.5") ;
+		$(this).css("filter", "alpha(opacity=50)") ;
+		$(this).css("cursor","auto");
+	}) ;
+	$("#zoom-in").click(function() {
+		zoom(zoom_factor+0.1) ;
+	}) ;
+}
+
+function zoom(factor) {
+	
+}
 
 function onWindowResize() {
 	// put content in the center
