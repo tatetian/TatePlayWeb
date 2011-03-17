d24bffd1a74af6499cf1a29e5eefd5506e35f5ad
Tate Tian <tatetian@gmail.com>
2011-03-17 12:53:16 +0800

    Text selection feature first commit

--- js/reader.js
+++ js/reader.js
@@ -5,21 +5,56 @@ var reader = {
 	zoomFactor: 1.00,
 	zoomMode: 'normal',	// 3 possible values: normal, fit-width, fit-height
 	docId: 0,
-	pageNum: 0,
+	pageNum: 1, // starting from 1, not 0
 	pages: 3,
-	empty: true
+	empty: true,
+	selectedArea: undefined,
+	selectingPrecision: 8	// text selection precision
+} ;
+
+function localPos($container, pageX, pageY) {
+	var originPageX = $container.offset().left ;
+	var originPageY = $container.offset().top ;
+	return {x: pageX - originPageX, y: pageY - originPageY} ;
 } ;
 
 /*
  * init elements on load event  
  * */
 $(document).ready(function(){
+	// load the pdf
+	loadPage(reader.docId, reader.pageNum) ;
+	// selected area
+	$selectedArea= $('<div id="selected_area" style="border:2px #333333 dashed; position:absolute; width:0; height:0;"></div>') ;
+	$("#viewport").append($selectedArea) ;
+	reader.selectedArea = $selectedArea;
 	// add event handlers to controls
 	addEventHandlerToControls() ;
 	// handle window resize event
 	$(window).resize(onWindowResize) ;
-	// load the pdf
-	loadPage(reader.docId, reader.pageNum) ;
+	// text selecting event
+	$("#viewport-background").mousedown(function(e){e.preventDefault();});
+	$("#viewport").mousedown(function(e) {
+		var localOriginPos = localPos($(this), e.pageX, e.pageY) ;
+		var $selectedArea = reader.selectedArea ;
+		$selectedArea.css("left", localOriginPos.x);
+		$selectedArea.css("top",localOriginPos.y);
+		$selectedArea.show();
+		$("#viewport").mousemove(localOriginPos, function(e) {
+			var localNowPos = localPos($(this), e.pageX, e.pageY) ;
+			var localOriginPos = e.data ;
+			var $selectedArea = reader.selectedArea ;
+			$selectedArea.width(localNowPos.x - localOriginPos.x);
+			$selectedArea.height(localNowPos.y - localOriginPos.y);
+		}) ;
+	});
+	$("#viewport").mouseup(function(e) {
+		$(this).unbind("mousemove");
+		var $selectedArea = reader.selectedArea ;
+		$selectedArea.width(0);
+		$selectedArea.height(0);
+		$selectedArea.hide();
+	}) ;
 }) ;
 
 function loadPage(docId, pageNum) {
@@ -37,8 +72,8 @@ function loadPage(docId, pageNum) {
     		"/api/pdf_img.php?doc_id="+docId+"&page_num="+pageNum);
     // load pdf data
 	$.get("/api/pdf_data.php", {doc_id: docId, page_num:pageNum}, function(data) {		
-		var width = data.pages[reader.pageNum].pageWidth;
-        var height = data.pages[reader.pageNum].pageHeight;
+		var width = data.pages[reader.pageNum-1].pageWidth;
+        var height = data.pages[reader.pageNum-1].pageHeight;
         $("#viewport").width(width);
         $("#viewport").height(height);
         $("#main").show(800);
