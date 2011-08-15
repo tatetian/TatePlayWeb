0e5dff9b6444ba6fa5c7b96855fb9b0f35bbc4c2
tatetian <tatetian@gmail.com>
2011-08-16 01:12:50 +0800

    Disable background image selection

--- api/pdf_img.php
+++ api/pdf_img.php
@@ -1,5 +1,5 @@
 <?php
-$file = 'content/page-0'.$_GET['page_num'].'.png';
+$file = 'content/page-'.$_GET['page_num'].'.png';
 header("Content-length:" . filesize($file));
 header("Content-type:image/png");
 readfile($file);
